ea72cf25a98ea65443efc434af8cd7e585dac913
aaryannemade <a.nemade@outlook.com>
2024-11-24 12:04:35 +0100

    Added links to timeline

---
+++ externalLinks.js
@@ -0,0 +1,119 @@
+﻿//All commit Messages
+var southWestCord = L.latLng(83.44378, -150.16663);
+var northEatCord = L.latLng(83.71254, -145.84351);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://devolution6.glitch.me/dicey_evolution.html", '_blank').focus();
+});
+
+//7drl play here
+var southWestCord = L.latLng(71.98748, -154.02832);
+var northEatCord = L.latLng(73.27893, -147.10144);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://terrycavanaghgames.com/dice/devolution/7drl/index.html", '_blank').focus();
+});
+
+//v0.3 play here
+var southWestCord = L.latLng(71.98748, -108.22357);
+var northEatCord = L.latLng(73.27893, -101.30219);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://terrycavanaghgames.com/dice/devolution/v0.3/index.html", '_blank').focus();
+});
+
+//v0.8 play here 
+var southWestCord = L.latLng(71.98748, -58.97461);
+var northEatCord = L.latLng(73.27893, -51.36658);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://terrycavanaghgames.com/dice/devolution/v0.8/index.html", '_blank').focus();
+});
+
+//Youtube: Teaser 
+var southWestCord = L.latLng(82.07306, -35.06836);
+var northEatCord = L.latLng(83.26483, -22.23633);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.youtube.com/watch?v=xShpQ85jCVE", '_blank').focus();
+});
+
+//v0.12 Play Here
+var southWestCord = L.latLng(71.98748, -4.36707);
+var northEatCord = L.latLng(73.27893, 2.5433);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://terrycavanaghgames.com/dice/devolution/v0.12/index.html", '_blank').focus();
+});
+
+//Youtube: v0.12 Beta Music
+var southWestCord = L.latLng(78.24579, 8.5199);
+var northEatCord = L.latLng(78.79024, 12.18658);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.youtube.com/watch?v=oCN8pV82lwo", '_blank').focus();
+});
+
+//v0.16 Play Here
+var southWestCord = L.latLng(71.98748, 49.021);
+var northEatCord = L.latLng(73.27893, 55.97534);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://terrycavanaghgames.com/dice/devolution/v0.16/index.html", '_blank').focus();
+});
+
+//Youtube: v0.16 New Music
+var southWestCord = L.latLng(78.23684, 60.67749);
+var northEatCord = L.latLng(78.84563, 64.75616);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.youtube.com/watch?v=eKUAqS7mFrI&t=3s", '_blank').focus();
+});
+
+//Youtube: Steam Launch Trailer
+var southWestCord = L.latLng(82.04574, 78.59619);
+var northEatCord = L.latLng(83.24677, 91.18652);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.youtube.com/watch?v=E2AdLWsRuHg&t=1s", '_blank').focus();
+});
+
+//v1.8.2 Play here
+var southWestCord = L.latLng(71.98748, 135.90637);
+var northEatCord = L.latLng(73.27893, 142.86072);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://terrycavanaghgames.com/dice/devolution/launched/index.html", '_blank').focus();
+});
+
+//Youtube: Megaquest Trailer
+var southWestCord = L.latLng(79.62208, 147.20581);
+var northEatCord = L.latLng(80.54832, 154.39087);
+var bounds = [southWestCord, northEatCord];
+var rectangle = L.rectangle(bounds, {
+    color: "#66000000"
+}).addTo(map).on('click', function(){
+    window.open("https://www.youtube.com/watch?v=rBEjzw8YBRo", '_blank').focus();
+});
