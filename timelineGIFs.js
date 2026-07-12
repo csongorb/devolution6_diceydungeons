91398c6865218e8c21166610da0da65a22efb87a
Ramninder <ramninder@ue-germany.de>
2026-07-12 17:18:27 +0200

    All gifs added to the timeline

--- timelineGIFs.js
+++ timelineGIFs.js
@@ -27,6 +27,176 @@ var gifList = [
         url: 'Timeline-content/gifs/ratking.gif',
         bounds: [[-12.83, 4.36], [-6.16, 16.40]],
         minZoom: 5
+    },
+    {
+        url: 'Timeline-content/gifs/newui.gif',
+        bounds: [[-3.63, -64.40], [0.01, -57.95]],
+        minZoom: 6
+    },
+    {
+        url: 'Timeline-content/gifs/banshee.gif',
+        bounds: [[-18.54, -64.41], [-14.92, -57.66]],
+        minZoom: 6
+    },
+    {
+        url: 'Timeline-content/gifs/tapcontrols4.gif',
+        bounds: [[3.32, 26.56], [5.47, 30.40]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/wizard4.gif',
+        bounds: [[-0.27, 26.58], [1.89, 30.42]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/bandit4.gif',
+        bounds: [[-7.39, 26.59], [-5.23, 30.47]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/skeleton4.gif',
+        bounds: [[-10.92, 26.63], [-8.82, 30.44]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/snowman62.gif',
+        bounds: [[0.28, 38.82], [2.42, 42.62]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/limitbreak64.gif',
+        bounds: [[-3.32, 38.81], [-1.21, 42.57]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/rogue63.gif',
+        bounds: [[-10.89, 38.71], [-8.75, 42.58]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/hardmode5,4.gif',
+        bounds: [[-19.57, 32.84], [-17.54, 36.68]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/thiefvswolfpuppy5,2.gif',
+        bounds: [[-25.96, 32.84], [-23.95, 36.78]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/apprentice51.gif',
+        bounds: [[-28.94, 32.85], [-27.01, 36.75]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/71.gif',
+        bounds: [[-16.36, 46.13], [-14.26, 49.68]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/82.gif',
+        bounds: [[-12.60, 92.98], [-7.27, 102.60]],
+        minZoom: 5
+    },
+    {
+        url: 'Timeline-content/gifs/81.gif',
+        bounds: [[-23.81, 106.67], [-19.35, 115.21]],
+        minZoom: 5
+    },
+    {
+        url: 'Timeline-content/gifs/newspellbook.gif',
+        bounds: [[-18.89, 127.75], [-15.86, 133.41]],
+        minZoom: 6
+    },
+    {
+        url: 'Timeline-content/gifs/cursed.gif',
+        bounds: [[-18.88, 135.71], [-15.85, 141.37]],
+        minZoom: 6
+    },
+    {
+        url: 'Timeline-content/gifs/newwitch.gif',
+        bounds: [[-14.97, 205.48], [-8.46, 217.36]],
+        minZoom: 5
+    },
+    {
+        url: 'Timeline-content/gifs/animdd.gif',
+        bounds: [[-42.35, 217.63], [-40.45, 222.12]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/overworld2.gif',
+        bounds: [[-42.26, 226.93], [-40.45, 232.16]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/jesteranimate.gif',
+        bounds: [[-26.82, 241.99], [-24.52, 246.55]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/jestergameplay.gif',
+        bounds: [[-30.75, 242.03], [-28.57, 246.51]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/soytari.gif',
+        bounds: [[-1.62, 254.70], [0.65, 258.76]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/scathach.gif',
+        bounds: [[-6.05, 254.71], [-3.77, 258.76]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/shovel.gif',
+        bounds: [[-1.33, 268.36], [1.08, 272.64]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/youwin.gif',
+        bounds: [[-17.82, 268.28], [-15.41, 272.76]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/cleverhandyman.gif',
+        bounds: [[-22.29, 268.14], [-19.87, 272.76]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/rosebattle.gif',
+        bounds: [[-26.72, 268.13], [-24.38, 272.76]],
+        minZoom: 7
+    },
+    {
+        url: 'Timeline-content/gifs/bambam.gif',
+        bounds: [[-17.87, 310.33], [-13.63, 318.18]],
+        minZoom: 5
+    },
+    {
+        url: 'Timeline-content/gifs/amdicenow.gif',
+        bounds: [[-25.37, 314.07], [-21.15, 322.21]],
+        minZoom: 5
+    },
+    {
+        url: 'Timeline-content/gifs/newsnap.gif',
+        bounds: [[-17.20, 328.59], [-12.04, 338.08]],
+        minZoom: 5
+    },
+    {
+        url: 'Timeline-content/gifs/D5hweR.gif',
+        bounds: [[-23.91, 405.76], [-17.68, 417.61]],
+        minZoom: 6
+    },
+    {
+        url: 'Timeline-content/gifs/D380My.gif',
+        bounds: [[-22.89, 434.89], [-17.10, 445.87]],
+        minZoom: 6
+    },
+    {
+        url: 'Timeline-content/gifs/DiceyDungeon_v.1.8_loop_min.gif',
+        bounds: [[-12.86, 488.21], [-4.19, 503.83]],
+        minZoom: 5
     }
 ];
 
