d73335ae9995bb92c699ccf9cf68990f0f260538
Ramninder <ramninder04de@gmail.com>
2026-07-27 22:31:31 +0200

    automatic youtube video pause fix

--- iframe.js
+++ iframe.js
@@ -1,21 +1,27 @@
-const iframe = document.getElementById("ytPlayer");
-
 function pauseVideo() {
     try{
-        const innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-        
-        const ytIframe = innerDoc.querySelector('iframe[src*="youtube"]');
-        
-        if (ytIframe && ytIframe.contentWindow) {
-            ytIframe.contentWindow.postMessage(
-                JSON.stringify({
-                    event: "command",
-                    func: "pauseVideo",
-                    args: []
-                }),
-                "*"
-            );
-        }
+        const sidebarIframes = document.querySelectorAll('.sidebar-content');
+
+        sidebarIframes.forEach(function (iframe) {
+            const innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+            if (!innerDoc) {
+                return;
+            }
+
+            const ytIframe = innerDoc.querySelector('iframe[src*="youtube"]');
+
+            if (ytIframe && ytIframe.contentWindow) {
+                ytIframe.contentWindow.postMessage(
+                    JSON.stringify({
+                        event: "command",
+                        func: "pauseVideo",
+                        args: []
+                    }),
+                    "*"
+                );
+            }
+        });
     }
     catch (error) {
         console.warn("Error pausing video:", error);
